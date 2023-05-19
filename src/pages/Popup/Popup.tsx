946c6640fed423c66b473829f020ac710878d22c
angusho1 <57027339+angusho1@users.noreply.github.com>
2023-05-18 22:59:53 -0700

    Enable keyboard shortcut to trigger scan

--- src/pages/Popup/Popup.tsx
+++ src/pages/Popup/Popup.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Popup.css';
 import { extractText, insertHtml, scanPage } from '../../scripts/images';
 
 const Popup = () => {
+  const [isScanning, setIsScanning] = useState<boolean>();
+
   const scanImages = async () => {
+    setIsScanning(true);
     const [tab] = await chrome.tabs.query({ active: true });
     const scriptTargetOptions = {
       tabId: tab.id as number,
@@ -27,7 +30,7 @@ const Popup = () => {
           .then(res => {
             console.log(res.symbols);
             const symbols = res.symbols
-              .filter(symbol => symbol.confidence > 90)
+              .filter(symbol => symbol.confidence > 95)
               .map(symbol => ({ bbox: symbol.bbox, text: symbol.text }));
 
             chrome.scripting.executeScript({
@@ -35,15 +38,25 @@ const Popup = () => {
               func: insertHtml,
               args: [imgSrc, symbols]
             });
-          });
+          })
+          .then(() => setIsScanning(false))
       });
     });
   };
 
+  useEffect(() => {
+    scanImages();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={scanImages}>Scan Images</button>
+        <div>
+          { isScanning ? 'Scanning Images...' : null }
+        </div>
+        { !isScanning && (
+          <button onClick={scanImages}>Scan Images</button>
+        )}
       </header>
     </div>
   );
